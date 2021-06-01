cca70b62c5348b7a0b30ee12f666211d22c01b7a
Ohad Keshet <53540070+ohadke@users.noreply.github.com>
2021-06-01 11:31:15 +0300

    Add files via upload

---
+++ Day 15/AngularMiniProjects/Day 15/AngulerPopQuiz/src/app/model/questions.ts
@@ -0,0 +1,34 @@
+import { Question } from "./question";
+
+export const QUESTIONS: Question[] = [
+    {
+        caption: 'What is Monica\'s biggest pet peeve?',
+        answers: ['Animals dressed as humans','Michael Flatley, Lord of the Dance','Scarecrows','Baby Dolls'],
+        correctAnswer: 0,
+        userAnswer: -1
+    },
+    {
+        caption: 'According to Chandler, what phenomenon scares the bejesus out of him?',
+        answers: ['Cows','Unicorns','Michael Flatley, Lord of the Dance','Animals dressed as humans'],
+        correctAnswer: 2,
+        userAnswer: -1
+    },
+    {
+        caption: 'Monica and Ross had a grandmother who died. Name that grandmother!',
+        answers: ['Althea','Cordelia','Harriet','Elsie'],
+        correctAnswer: 0,
+        userAnswer: -1
+    },
+    {
+        caption: 'Every week the TV Guide comes to Chandler and Joey\'s apartment. What name appears on the address label?',
+        answers: ['Joey Tribbiani','Ken Adams','Chandler Bing','Ms. Chanandler Bong'],
+        correctAnswer: 3,
+        userAnswer: -1
+    },
+    {
+        caption: 'What is the name of Chandler\'s father\'s Las Vegas all-male burlesque?',
+        answers: ['Arissa','Independancy Injection','Viva Las Gaygas','The Big Gang'],
+        correctAnswer: 2,
+        userAnswer: -1
+    },
+]
